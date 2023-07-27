ef45b16ec6939e7b58cad3d7ef91006d260a70d7
SaileshReddyM <saileshreddy789@gmail.com>
2023-07-27 14:08:59 +0530

    add raw and interim data & files

---
+++ data_consolidation_interim.py
@@ -0,0 +1,53 @@
+import glob
+import pandas as pd 
+from pathlib import Path
+
+
+def get_interim_data():
+
+    project_dir = Path(__file__).parent    
+    raw_data_dir = project_dir / "raw_data"             
+    interim_data_dir = project_dir / "interim_data"
+
+    interim_data_dir.mkdir(parents=True,exist_ok=True)
+
+    table_names = ["wap_tea_city","wap_tea_india","wap_leaf_and_dust_city","wap_leaf_and_dust_india"]
+
+    for table_name in table_names:
+         
+        table_data_all_years = [ pd.read_csv(file)
+                            for file in glob.glob(f"./raw_data/**/{table_name}_*.csv")]
+        
+        if "india" in table_name:
+            new_column_names = {
+                    '0': 'Week Ending/Date',
+                    '1': 'NORTH INDIA',
+                    '2': 'SOUTH INDIA',
+                    '3': 'ALL INDIA',
+                }
+            for df in table_data_all_years:
+                df.rename(columns=new_column_names, inplace=True)
+                df.drop(index=[0,1], inplace=True)
+                df.reset_index(drop=True, inplace=True)
+        
+        table_data_all_years_melted = [ pd.melt(df, id_vars='Week Ending/Date', var_name='Location', value_name='Average_Price')
+                            for df in table_data_all_years]
+        
+        concat_table = pd.concat(table_data_all_years_melted)
+
+        column_names_in_table = concat_table.columns
+
+        for column_name in column_names_in_table:
+            if(column_name == "Average_Price"):
+                concat_table[f'bv_{column_name}'] = concat_table[column_name].str.extract(r'\((.*?)\)')
+                concat_table[f'ov_{column_name}'] = concat_table[column_name].str.extract(r'^(.*?)\(')
+                concat_table.drop(columns=column_name, inplace=True)
+
+        print(concat_table)
+        concat_table.to_csv(f"./interim_data/{table_name}.csv")
+
+
+
+
+
+get_interim_data()
---
+++ rawdata_capture.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd 
+import os
+
+def table_name(i,this_year):
+    if i == 0:
+        return f"{this_year}_data/wap_leaf_and_dust_city_{this_year}.csv"
+    if i == 1:
+        return f"{this_year}_data/wap_tea_city_{this_year}.csv"
+    if i == 2:
+        return f"{this_year}_data/wap_leaf_and_dust_india_{this_year}.csv"
+    if i == 3:
+        return f"{this_year}_data/wap_tea_india_{this_year}.csv"
+    else:
+        return f"{this_year}_data/additional_table.csv"
+
+def get_year_to_year_data(year1,year2):
+    
+    start_year = year1
+    end_year = year2
+
+    # total_data = pd.DataFrame([], columns=['week ending/date','location','average_price'])
+
+    for year_index in range(start_year,end_year+1):
+        res = requests.get(f'https://www.teaboard.gov.in/WEEKLYPRICES/{year_index}')
+
+        if(res.status_code != 200 ):
+            print(f"Could not gather the data successfully from the URL of year: {year_index}")
+        else:
+
+            if not os.path.exists(f"{year_index}_data"):
+                os.mkdir(f"{year_index}_data")
+            
+            df_list = pd.read_html(res.content)
+
+            for i in range(0,len(df_list)-1):
+                df_list[i].to_csv(table_name(i,year_index),index=False)
+            
+            
+
+get_year_to_year_data(2008,2023)
+
